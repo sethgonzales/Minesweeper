a92e5c8b43e3ef1e07fdc5df81d086fd40df7277
ian-bravo <bravo.ian@gmail.com>
2023-09-11 16:30:51 -0700

    create mine function
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>
    Co-authored-by: Richard Barbour <richyb07@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-// import { createGameBoard } from "./js/game-logic.js";
 import './css/styles.css';
 
 //Business Logic
-  function createGameBoard(boardSize) {
-    const board = [];
-    
-    for (let x = 0; x < boardSize; x++) {
-      const row = [];
-      for (let y = 0; y < boardSize; y++) {
-        
-        
-        const tile = {
-          x,
-          y,
-          hidden: true,
-          mine: false,
-          flag: false,
-          // number,
-        };
-
-        createDiv(tile);
-
-        row.push(tile);
-      }
-      board.push(row);
-   
-    }
-  console.log(board);
-    return board;
-  
+function createGameBoard(boardSize, numberOfMines) {
+const board = [];
+for (let x = 0; x < boardSize; x++) {
+  const row = [];
+  for (let y = 0; y < boardSize; y++) {
+    const tile = {
+      x,
+      y,
+      hidden: true,
+      mine: false,
+      flag: false,
+    };
+    createDiv(tile);
+    row.push(tile);
   }
+  board.push(row);
+}
+console.log(board);
+setMinePosition (board, numberOfMines);
+return board;  
+}
 
-// createGameBoard(10);
-
+function setMinePosition (board, numberOfMines) {
+  let minesPlaced = 0;
+  
+  while (minesPlaced < numberOfMines) {
+    const x = Math.floor(Math.random() * boardSize);
+    const y = Math.floor(Math.random() * boardSize);
+  
+    if (!board[x][y].tile.mine === true) {
+      board[x][y].tile.mine = true;
+      minesPlaced++;
+    }// not working figure out tomorrow
+}
+}
+  
 //UI Logic 
-// const board = createGameBoard(10);
-
-
 function createDiv(tile) {
   const element = document.createElement("div");
   tile.element = element;
@@ -48,23 +49,5 @@ function createDiv(tile) {
 }
 
 const boardSize = 5;
-createGameBoard(boardSize);
-
-
-// const board = createGameBoard(10);
-// const boardElement = document.querySelector(".board");
-
-// board.forEach(row => {
-//   row.forEach(tile => {
-//     boardElement.append(tile.element);
-//     // tile.element.addEventListener(click), () => {
-//     //   tile.hidden = false;
-//     })
-//   });
-
-
-// function setStatusHidden(element) {
-//   element.setAttribute("class", "hidden");
-// }
-
-// document.getElementsByClassName("first-class")[0].removeAttribute("class", "hidden");
+const numberOfMines = 10;
+createGameBoard(boardSize, numberOfMines);
