082e986aef85871dcf231b8a709b94888930c956
ian-bravo <bravo.ian@gmail.com>
2023-09-12 15:14:08 -0700

    show an array of adj tiles and an array of adj mines
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>
    
    Co-authored-by: Richard Barbour <richyb07@gmail.com>
    
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ for (let x = 0; x < boardSize; x++) {
       mine: false,
       flag: false,
     };
-    createDiv(boardSize, tile);
+    createDiv(boardSize, tile, board);
     row.push(tile);
   }
   board.push(row);
@@ -21,6 +21,34 @@ for (let x = 0; x < boardSize; x++) {
 setMinePosition (board, numberOfMines);
 console.log(board);
 return board;  
+}
+
+function nearbyTile ({x, y}, board) {
+let surroundingTiles = [];
+// const nearbyTileLocation = []
+for ( let xNearBy = -1; xNearBy <= 1; xNearBy ++) {
+  for ( let yNearBy = -1; yNearBy <= 1; yNearBy ++) {
+    const newX = x + xNearBy;
+    const newY = y + yNearBy;
+
+    if (newX >= 0 && newX < board.length && newY >= 0 && newY < board.length) {
+     const nearTile = board[newX][newY];
+      surroundingTiles.push(nearTile);
+    }
+  }
+}
+  checkAdjacentTiles(surroundingTiles);
+}
+
+function checkAdjacentTiles(surroundingTiles) {
+  //create an array of surrounding tiles  
+
+      // EXAMPLE: adjTiles = [t1.mine=t, t2.mine=f, t3.mine=f]
+  
+  //look through array and filter to create array of tiles with mines =true
+ const mines = surroundingTiles.filter(tile => tile.mine===true);
+ console.log(mines);
+//       EXAMPLE: mines = [t1.mine=t]
 }
 
 function setMinePosition (board, numberOfMines) {
@@ -36,30 +64,43 @@ function setMinePosition (board, numberOfMines) {
 }
   
 //UI Logic 
-function createDiv(boardSize, tile) {
+function createDiv(boardSize, tile, board) {
   const element = document.createElement("div");
   tile.element = element;
   tile.element.addEventListener("click", function() {
-    revealTile(tile);
+    nearbyTile(tile, board);
+    revealTile(tile, board);
   });
   const boardElement = document.querySelector(".board");
   boardElement.style.setProperty("--size", boardSize);
-  boardElement.append(tile.element);
+  boardElement.append(tile.element); 
   return element;
 }
+
+
+  // //look through the mines array 
+  //     if (mines.length === 0) { //if the array contains NO mines
+  //         adjTiles.forEach(revealTile()) //loop through the array of adjacent tiles and reveal them if they also do not have adjacent mines.
+  //     else { //for any tile that does have adjacent mines
+  //         tile.element.innerHTML = 
+  //         <p>${mines.length}</p> //update the tile to have the number of mines, found by the lenth of the mines array.
+  //     }
+  //     }
+  //   }
+
+
 
 function revealTile(tile) {
   
   if (tile.mine === true) {
     tile.element.setAttribute("class", "bomb");
-    console.log(tile.mine);
-    //change CSS color to red for that tile
     // game over
   } else {
-    console.log("else");
-    // reveal the square
+    tile.element.setAttribute("class", "revealed");
+    tile.hidden = false;
+    console.log(tile.hidden);
   }
-  console.log("you workin");
+  // console.log(board); //remove board console logs and then from revealTile, createDiv param and argument. 
 }
 
 
